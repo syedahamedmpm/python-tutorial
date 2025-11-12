f43a12342de581a2db063983c6a98d185a4faecf
Syed Ahamed <salearning01@gmail.com>
2025-11-12 07:43:53 +0530

    DAY4 Activities

---
+++ DAY4/ifelif.py
@@ -0,0 +1,17 @@
+mark = 99
+
+if mark <=59:
+    print('Grade E')
+
+elif mark <=69:
+    print('Grade D')
+
+elif mark<=79:
+    print('Grade C')
+
+elif mark<=89:
+    print('Grade B')
+elif mark<=99:
+    print('Grade S')
+else:
+    print('Failed')
